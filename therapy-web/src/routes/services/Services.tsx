142425388023b1fe355248dd6cc4bcbdacbc309e
abdullahramzan0900 <abdullahramzan0900@gmail.com>
2026-07-09 00:35:22 +0100

    changes

--- therapy-web/src/routes/services/Services.tsx
+++ therapy-web/src/routes/services/Services.tsx
@@ -4,7 +4,7 @@ import IndividualTherapy from "../../components/OurServices/OurServices";
 import "./Services.scss";
 import TherapySupport from "../../components/therapySupport/TherapySupport";
 import TherapyProcess from "../../components/GuidingPrinciples/GuidingPrinciples";
-import WhoWesupport from "../../components/WhoWesupport/WhoWeSupport";
+import WhoWesupport from "../../components/WhoWesupport/WhoWesupport";
 
 const Services = () => {
   return <>
