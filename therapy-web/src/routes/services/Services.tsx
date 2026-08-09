06737e749caf46131efd7855e354162c11de03cb
abdullahramzan0900 <abdullahramzan0900@gmail.com>
2026-08-10 00:51:22 +0100

    chnages

--- therapy-web/src/routes/services/Services.tsx
+++ therapy-web/src/routes/services/Services.tsx
@@ -4,14 +4,13 @@ import IndividualTherapy from "../../components/OurServices/OurServices";
 import "./Services.scss";
 import TherapySupport from "../../components/therapySupport/TherapySupport";
 import TherapyProcess from "../../components/GuidingPrinciples/GuidingPrinciples";
-import WhoWeSupport from "../../components/WhoWeSupport/WhoWeSupport";
 
 const Services = () => {
   return <>
 <TherapySupport/>
    {/* <HomeCareSection/> */}
   <IndividualTherapy/>
- <WhoWeSupport/>
+ {/* <WhoWeSupport/> */}
    {/* <OurServices/> */}
    {/* <ServicesSection isCarousel ={false}/> */}
    <TherapyProcess/>
